3a1c9fc9f700a2383356ca0739e47a65a29a91db
HwangJunHa <jhhwghg9911@naver.com>
2023-02-12 18:26:48 +0900

    Change some notations

--- layers/LayerNorm.py
+++ layers/LayerNorm.py
@@ -12,11 +12,11 @@ class LayerNorm(nn.Module):
 
     def __init__(self, features, eps=1e-6):
         super(LayerNorm, self).__init__()
-        self.a_2 = nn.Parameter(torch.ones(features))  # scale
-        self.b_2 = nn.Parameter(torch.zeros(features))  # bias
+        self.gamma = nn.Parameter(torch.ones(features))  # scale
+        self.beta = nn.Parameter(torch.zeros(features))  # bias
         self.eps = eps
 
     def forward(self, x):
         mean = x.mean(-1, keepdim=True)
         std = x.std(-1, keepdim=True)
-        return self.a_2 * (x - mean) / (std + self.eps) + self.b_2
+        return self.gamma * (x - mean) / (std + self.eps) + self.beta
