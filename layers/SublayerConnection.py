85d654f90de83d80be91cbf6fc7a710cc8eaa9f6
HwangJunHa <jhhwghg9911@naver.com>
2023-02-12 15:51:22 +0900

    Create SublayerConnection.py

---
+++ layers/SublayerConnection.py
@@ -0,0 +1,14 @@
+import torch.nn as nn
+
+class SublayerConnection(nn.Module):
+    """
+    Residual Connection
+    """
+
+    def __init__(self, size, dropout):
+        super(SublayerConnection, self).__init__()
+        self.norm = LayerNorm(size)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, sublayer):
+        return x + self.dropout(sublayer(self.norm(x)))
