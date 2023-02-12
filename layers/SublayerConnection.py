71dc89d57e6b820a9b3547bd7c35917faf0047f5
HwangJunHa <jhhwghg9911@naver.com>
2023-02-12 16:01:25 +0900

    Update some comments

--- layers/SublayerConnection.py
+++ layers/SublayerConnection.py
@@ -4,12 +4,18 @@ from Transformer.layers.LayerNorm import LayerNorm
 class SublayerConnection(nn.Module):
     """
     Residual Connection + Layer Normalization
+
+    Note that it was updated after writing the paper about below things
+
+    1. output is changed to x + SubLayer(LayerNorm(x))
+    2. Layer Normalization is added the final outputs of encoder/decoder at last
     """
 
-    def __init__(self, size, dropout):
+    def __init__(self, size, d_rate):
         super(SublayerConnection, self).__init__()
         self.norm = LayerNorm(size)
-        self.dropout = nn.Dropout(dropout)
+        self.dropout = nn.Dropout(d_rate)
 
     def forward(self, x, sublayer):
+        # According to the paper, apply dropout to the output of each sub-layer
         return x + self.dropout(sublayer(self.norm(x)))
