0c034ce8c5727a662b701d9c91afff6f0a2248fe
HwangJunHa <jhhwghg9911@naver.com>
2023-02-12 17:10:09 +0900

    Create Decoder.py

---
+++ model/Decoder.py
@@ -0,0 +1,24 @@
+import torch.nn as nn
+import copy
+from Transformer.layers.LayerNorm import LayerNorm
+
+class Decoder(nn.Module):
+    """Decoder is consist with N-layers"""
+
+    def __init__(self, decoder_block, N):
+        super(Decoder, self).__init__()
+        self.layers = nn.ModuleList([copy.deepcopy(decoder_block) for _ in range(N)])  # N-Encoder Blocks
+        self.norm = LayerNorm(decoder_block.size)
+
+    def forward(self, x, memory, src_mask, tgt_mask):
+        """
+        :param x:
+        :param memory:
+        :param src_mask:
+        :param tgt_mask:
+        :return:
+        """
+        for layer in self.layers:
+            x = layer(x, memory, src_mask, tgt_mask)
+
+        return self.norm(x)
